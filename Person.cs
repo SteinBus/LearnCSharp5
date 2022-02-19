11d43d73b7723cbc0fe60c3605d8260bd42afe2a
Alex <bsgnlksy@gmail.com>
2022-02-19 09:40:16 +0300

    added person class

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp5
+{
+    internal class Person
+    {
+        private int _age;
+
+        public int GetAge()
+        {
+            return _age;
+        }
+        public void SetAge(int age)
+        {
+            if ( age < 0 )
+            {
+                throw new ArgumentOutOfRangeException($"age({age}) can't be negative");
+            }
+        }
+    }
+}
